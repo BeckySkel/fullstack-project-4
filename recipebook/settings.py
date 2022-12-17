e39f73813f0cffaab8fa7c66cafa47174ba216c3
BeckySkel <beckyskelcher@hotmail.co.uk>
2022-12-17 12:34:59 +0000

    Fix css not loading in heroku app

--- recipebook/settings.py
+++ recipebook/settings.py
@@ -16,6 +16,8 @@ import dj_database_url
 from django.contrib.messages import constants as messages
 if os.path.isfile('env.py'):
     import env
+import mimetypes
+mimetypes.add_type("text/css", ".css", True)
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
