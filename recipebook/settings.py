6a25fbac3d71b389e4847778f7ab466a2c7ca04a
BeckySkel <beckyskelcher@hotmail.co.uk>
2022-12-27 01:03:23 +0000

    Change debug to test on heroku

--- recipebook/settings.py
+++ recipebook/settings.py
@@ -31,8 +31,8 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-# DEBUG = ('DEBUGGING' in os.environ)
+# DEBUG = True
+DEBUG = ('DEBUGGING' in os.environ)
 
 ALLOWED_HOSTS = ['pp4-recipebook.herokuapp.com', 'localhost']
 
